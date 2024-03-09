d1be216012bea3737f08072369cbff3c6d5996f9
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2024-03-09 22:48:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -55,7 +55,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/RitikKaal/EsproxMusicBot",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "ritik")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # ----------------------------------------------------------------
