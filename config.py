0bbf6d23d5b9d69dd54ef59f532f8ea5c817918f
WEREWOLF-DEMON <153902241+WEREWOLF-DEMON@users.noreply.github.com>
2024-06-01 00:46:03 +0530

    🎵🎶ꜱᴡᴇᴇᴛ_ᴛᴏxɪᴄ_ᴍᴜꜱɪᴄ🩸🖤🫀

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/SweetToxicDevil/EsproAiMusicBot",
+    "https://github.com/WEREWOLF-DEMON/EsproAiMusicBot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "ritik")
 GIT_TOKEN = getenv(
